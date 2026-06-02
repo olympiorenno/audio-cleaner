b9e01e261f565549ff5485ea2cc5e58912cf2030
olympiorenno <olympiorenno@github.com>
2026-06-02 16:32:04 -0300

    run.bat versao inicial simples

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\Users\Olympio\AppData\Local\Python\pythoncore-3.14-64\python.exe audio_cleaner.py
+C:\Users\Olympio\AppData\Local\Python\pythoncore-3.14-64\python.exe "%~dp0audio_cleaner.py"
 pause
