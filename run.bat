e0c301df071e6f81e4b6929c2c84755b33f9b872
olympiorenno <olympiorenno@github.com>
2026-06-02 18:12:53 -0300

    run.bat mata processo anterior antes de iniciar

--- run.bat
+++ run.bat
@@ -1,3 +1,10 @@
 @echo off
+set HF_HOME=C:\ProgramData\AudioCleaner\models
+
+REM Mata processos anteriores do audio_cleaner para nao sobrepor
+taskkill /F /IM python.exe /FI "WINDOWTITLE eq audio_cleaner*" >nul 2>&1
+taskkill /F /FI "IMAGENAME eq python.exe" /FI "MEMUSAGE gt 100000" >nul 2>&1
+
+REM Inicia o script
 C:\Users\Olympio\AppData\Local\Python\pythoncore-3.14-64\python.exe "%~dp0audio_cleaner.py"
 pause
