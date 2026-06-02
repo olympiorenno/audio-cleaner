b2d0adcb356bcba67a9892862e9667e6a1ba885a
olympiorenno <olympiorenno@github.com>
2026-06-02 20:35:25 -0300

    adiciona servidor WebSocket para extensao Chrome

---
+++ server.bat
@@ -0,0 +1,5 @@
+@echo off
+set HF_HOME=C:\ProgramData\AudioCleaner\models
+echo Iniciando servidor Audio Cleaner...
+C:\Users\Olympio\AppData\Local\Python\pythoncore-3.14-64\python.exe "%~dp0server.py"
+pause
